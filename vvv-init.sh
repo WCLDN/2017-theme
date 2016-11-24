484b2ccd283df1a8b9c6459a696574a564a9715e
Matthew Duhig <matthew@wearefx.co.uk>
2016-11-24 22:19:31 +0000

    Lorelei's fail

--- vvv-init.sh
+++ vvv-init.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ln -s mu-plugin.php ../wordpress-meta-environment/wordcamp.dev/public_html/wp-content/mu-plugins/wcldn-2017.php
+cd ../wordpress-meta-environment/wordcamp.dev/public_html/wp-content/mu-plugins
+
+ln -sf ../../../../../../2017-theme/mu-plugin.php wcldn-2017.php
