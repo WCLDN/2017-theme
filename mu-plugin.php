1ee78192862312da09c2e8222b356aa08db15978
Matthew Duhig <matthew@wearefx.co.uk>
2016-12-01 19:52:46 +0000

    Add typekit font script to header

--- mu-plugin.php
+++ mu-plugin.php
@@ -5,3 +5,11 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_cached_css', 11 );  
 function enqueue_cached_css() {
 	wp_enqueue_style( 'wcldn-2017', 'http://wcldn-2017.dev/build/style.css' );
 }
+
+function typekit() {
+    ?>
+    <script src="https://use.typekit.net/epu4uzu.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+    <?php
+}
+add_action( 'wp_head', __NAMESPACE__ . '\typekit' );
